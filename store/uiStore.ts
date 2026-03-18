e7680a343642b18886d136457341b9f32bbec421
VeniciusBipo <viniciusbispo272@gmail.com>
2026-03-18 14:33:28 -0300

    fix: add edit playlist modal state to UIStore

--- store/uiStore.ts
+++ store/uiStore.ts
@@ -1,17 +1,29 @@
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 
+interface PlaylistToEdit {
+  id: string
+  name: string
+  description?: string
+  coverImage?: string | null
+  isPublic: boolean
+}
+
 interface UIStore {
   isSidebarOpen: boolean
   isCreatePlaylistModalOpen: boolean
+  isEditPlaylistModalOpen: boolean
   isAddSongModalOpen: boolean
   activePlaylistId: string | null
+  playlistToEdit: PlaylistToEdit | null
   searchQuery: string
 
   toggleSidebar: () => void
   setSidebarOpen: (open: boolean) => void
   openCreatePlaylistModal: () => void
   closeCreatePlaylistModal: () => void
+  openEditPlaylistModal: (playlist: PlaylistToEdit) => void
+  closeEditPlaylistModal: () => void
   openAddSongModal: (playlistId: string) => void
   closeAddSongModal: () => void
   setSearchQuery: (query: string) => void
@@ -22,8 +34,10 @@ export const useUIStore = create<UIStore>()(
     (set) => ({
       isSidebarOpen: true,
       isCreatePlaylistModalOpen: false,
+      isEditPlaylistModalOpen: false,
       isAddSongModalOpen: false,
       activePlaylistId: null,
+      playlistToEdit: null,
       searchQuery: '',
 
       toggleSidebar: () => set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
@@ -33,6 +47,12 @@ export const useUIStore = create<UIStore>()(
       openCreatePlaylistModal: () => set({ isCreatePlaylistModalOpen: true }),
 
       closeCreatePlaylistModal: () => set({ isCreatePlaylistModalOpen: false }),
+
+      openEditPlaylistModal: (playlist) =>
+        set({ isEditPlaylistModalOpen: true, playlistToEdit: playlist }),
+
+      closeEditPlaylistModal: () =>
+        set({ isEditPlaylistModalOpen: false, playlistToEdit: null }),
 
       openAddSongModal: (playlistId) =>
         set({ isAddSongModalOpen: true, activePlaylistId: playlistId }),
